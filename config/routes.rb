9a7d312c2a412046bb1e296c0d5806b74cc93eaf
julien legay <legayj@gmail.com>
2019-08-06 15:46:09 +0200

    CRUD app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # List tasks
+  get 'tasks',            to: 'tasks#index', as: :tasks
+
+  # Add a new task
+  get 'tasks/new',        to: 'tasks#new', as: :new_task
+  post 'tasks',           to: 'tasks#create'
+
+  # View details of a task
+  # 'show' needs to be after 'new' route
+  get 'tasks/:id',        to: 'tasks#show', as: :task
+
+  # Edit a task
+  get 'tasks/:id/edit',   to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id',      to: 'tasks#update'
+
+  # Remove a task
+  delete 'tasks/:id',     to: 'tasks#destroy'
 end
